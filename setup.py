20575cf13cafe19aa46c4e884d8e1072aedd1785
Jung Gyu Yoon <yjg30737@gmail.com>
2022-05-11 09:11:44 +0900

    Downgrade to 0.0.1, update setup.py and requirements.txt to upload this package to pypi and add description of it

--- setup.py
+++ setup.py
@@ -1,8 +1,15 @@
 from setuptools import setup, find_packages
+import codecs
+import os
+
+here = os.path.abspath(os.path.dirname(__file__))
+
+with codecs.open(os.path.join(here, "README.md"), encoding="utf-8") as fh:
+    long_description = "\n" + fh.read()
 
 setup(
     name='pyqt-auto-search-bar',
-    version='0.1.0',
+    version='0.0.1',
     author='Jung Gyu Yoon',
     author_email='yjg30737@gmail.com',
     license='MIT',
@@ -11,9 +18,11 @@ setup(
                   'pyqt_auto_search_bar.ico': ['search.svg']},
     description='PyQt auto search bar',
     url='https://github.com/yjg30737/pyqt-auto-search-bar.git',
+    long_description_content_type='text/markdown',
+    long_description=long_description,
     install_requires=[
         'PyQt5>=5.8',
-        'pyqt-resource-helper @ git+https://git@github.com/yjg30737/pyqt-resource-helper.git@main',
-        'pyqt-svg-label @ git+https://git@github.com/yjg30737/pyqt-svg-label.git@main'
+        'pyqt-resource-helper>=0.0.1',
+        'pyqt-svg-label>=0.0.1'
     ]
 )
